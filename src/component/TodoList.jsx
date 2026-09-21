dba24e0d6ec439a16f580ce910e6e234446bcb6e
윤소휘 <dbsthgnl30@naver.com>
2026-09-21 23:56:15 +0900

    검색기능

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -2,15 +2,32 @@ import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
-//목록을 보여줌
+//목록을 보여줌,검색기능
 const TodoList =({list,onDelete,onToggle}) =>{
- 
+  const [search, setSearch]=useState("");
+
+  //검색창에 글자를 칠때마다 그글자를기억
+  const  onchangeSearch =(e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult =() =>{
+    return search === ""
+    ? list :list.filter((it) => it.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+
     return(
      <div className="TodoList">
       <h4>Todo List 🌱</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input className="searchbar"
+       placeholder="검색어를 입력하세요" 
+        value={search}
+        onChange={onchangeSearch}
+       />
       <div className="list_wrapper">
-        {list.map((it)=>(<TodoItem key ={it.id} {...it} onDelete={onDelete} onToggle={onToggle}/>
+        {getSearchResult().map((it)=>(<TodoItem key ={it.id} {...it} onDelete={onDelete} onToggle={onToggle}/>
         ))}
       </div>
     </div>
